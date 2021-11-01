c580177bfaadd9047239a346d6d0ef73cce89080
Alisson Youssf <youssfbr@gmail.com>
2021-11-01 10:31:02 -0300

    Project backend created

---
+++ backend/src/test/java/com/github/youssfbr/vendas/VendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
